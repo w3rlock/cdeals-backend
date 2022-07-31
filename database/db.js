ef6674934b60afe861d311a508fec9f173c11431
Yuri <kim010698@gmail.com>
2022-07-31 23:35:49 +0600

    db config

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const {Pool} = require('pg');
 const pool = new Pool({
     user: 'postgres',
-    password: '',
+    password: 'root',
     host: 'localhost',
     port: 5432,
     database: 'cdeals'
